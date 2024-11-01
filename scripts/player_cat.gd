f8711afbd4f9459d8cb6196cd67e492015ae1769
thiagoyc <thiagoyc@gmail.com>
2024-11-01 11:30:02 -0300

    Trees Tile Set
    Changed the texture to match tilesets

--- scripts/player_cat.gd
+++ scripts/player_cat.gd
@@ -2,15 +2,16 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
-var casting = false
+const SPEED = 60.0
+const JUMP_VELOCITY = -100.0
 
+var casting = false
+signal cast_done()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta / 2
+		velocity += get_gravity() * delta / 3
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor() and not casting:
@@ -51,3 +52,4 @@ func _physics_process(delta: float) -> void:
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if animated_sprite_2d.animation == "begging":
 		casting = false
+		emit_signal("cast_done")
