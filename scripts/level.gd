05f662d4aa803210c7b0e2c8df4a6d4ade9d541b
thiagoyc <thiagoyc@gmail.com>
2024-11-20 17:38:21 -0300

    Globals update

--- scripts/level.gd
+++ scripts/level.gd
@@ -18,36 +18,36 @@ const tree_textures = [
 	"res://assets/Seasons pack/Spring/Trees/Trees_Spring.png"
 ]
 
-var i = 0
 var last_i
 
 func _ready():
-	i = 0
-	last_i = 0
+	last_i = -1
 	update_terrain()
 
 
 func _process(_delta: float) -> void:
-	if i != last_i:
-		last_i = i
+	if last_i != Globals.seasons_int:
+		last_i = Globals.seasons_int
 		update_terrain()
 	
 
 func update_terrain() -> void:
-	var floor_texture = load(season_textures[i])
+	# Ground texture
+	var floor_texture = load(season_textures[Globals.seasons_int])
 	var floor_source : TileSetSource = midground.tile_set.get_source(0)
 	if floor_source is TileSetAtlasSource:
 		(floor_source as TileSetAtlasSource).texture = floor_texture
-		
-	var tree_texture = load(tree_textures[i])
+	
+	# Background trees texture
+	var tree_texture = load(tree_textures[Globals.seasons_int])
 	var tree_source : TileSetSource = midground.tile_set.get_source(1)
 	if tree_source is TileSetAtlasSource:
 		(tree_source as TileSetAtlasSource).texture = tree_texture
 	
+	# Lake texture (water/ice)
 	var water_cells_array = midground.get_used_cells_by_id(3)
 	var ice_cells_array = midground.get_used_cells_by_id(4)
-	
-	if i == 2:
+	if Globals.seasons[Globals.seasons_int] == "Winter":
 		for cell in water_cells_array:
 			var atlas_coords = midground.get_cell_atlas_coords(cell)
 			midground.set_cell(cell, 4, atlas_coords)
@@ -61,5 +61,4 @@ func update_terrain() -> void:
 
 
 func _on_player_cat_cast_done() -> void:
-	i = (i + 1) % 4
-	get_tree().call_group("SeasonAffected", "next_season")
+	Globals.next_season()
