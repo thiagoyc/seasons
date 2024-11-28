c0d0160a2f6ab35439ac940c74bd39c71785b01e
thiagoyc <thiagoyc@gmail.com>
2024-11-28 02:44:04 -0300

    Honey + Bear

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -12,3 +12,6 @@ const seasons = [
 
 func next_season() -> void:
 	seasons_int = (seasons_int + 1) % 4
+
+func reset_season() -> void:
+	seasons_int = 0
---
+++ scripts/hive.gd
@@ -0,0 +1,36 @@
+extends Sprite2D
+
+@onready var hive_tree: Node2D = $"../.."
+@onready var bee_spawner: Node2D = $BeeSpawner
+@onready var bee_spawner_2: Node2D = $BeeSpawner2
+@onready var bee_spawner_3: Node2D = $BeeSpawner3
+
+var bee_scene = preload("res://scenes/bee.tscn")
+
+@onready var bee_spawners = [
+	bee_spawner,
+	bee_spawner_2,
+	bee_spawner_3
+]
+
+func _ready() -> void:
+	EventController.interaction.connect(_on_interaction)
+
+
+func _on_interaction(node: Node) -> void:
+	if node == self:
+		# Ataca o player se as abelhas estiverem acordadas
+		if Globals.seasons[Globals.seasons_int] == "Summer" || Globals.seasons[Globals.seasons_int] == "Spring":
+			spawn_bees()
+		# Pega mel se for outono
+		elif Globals.seasons[Globals.seasons_int] == "Autumn":
+			print("Você pegou mel!")
+
+
+func spawn_bees():
+	for id in bee_spawners.size():
+		var bee = bee_scene.instantiate() as Node2D
+		var bee_spawn_location = bee_spawners[id]
+		bee.scale = Vector2(0.333, 0.333)
+		bee.position = bee_spawn_location.position
+		add_child(bee)
